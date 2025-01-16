153cb549842729bcfc449011cd6f0192bf3c04c4
DavidUrbaez <david.urbaez@smartwires.com>
2025-01-16 16:25:37 -0500

    Updates

---
+++ precache-manifest.8de35b130b067f0080db53a29ba2a26b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ac44e70965ee699c3c99e26d0a0d7d",
+    "url": "/ReactLearning/index.html"
+  },
+  {
+    "revision": "275e2bdda72ef92c3e8f",
+    "url": "/ReactLearning/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4309d6dc123f3d8f746d",
+    "url": "/ReactLearning/static/js/2.6c811e04.chunk.js"
+  },
+  {
+    "revision": "275e2bdda72ef92c3e8f",
+    "url": "/ReactLearning/static/js/main.964b29ab.chunk.js"
+  },
+  {
+    "revision": "935a8490eeb6ae761c28",
+    "url": "/ReactLearning/static/js/runtime~main.dc71acf1.js"
+  }
+]);
---
+++ static/js/main.964b29ab.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{12:function(e,t,n){},13:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),l=n(2),o=n.n(l);n(12);var r=()=>c.a.createElement("div",{className:"flex min-h-screen items-center justify-center"},c.a.createElement("h1",{className:"text-4xl font-bold text-blue-600"},"Hello, World!"));o.a.createRoot(document.getElementById("root")).render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(r,null)))},3:function(e,t,n){e.exports=n(13)}},[[3,1,2]]]);
+//# sourceMappingURL=main.964b29ab.chunk.js.map
